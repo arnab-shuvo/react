78daefe91b565c127a82ff5fce81381f617c9293
arnab-shuvo <arnabshuvo430@gmail.com>
2023-08-01 15:00:39 +0100

    SQL function added

--- src/pages/dataBase.sql
+++ src/pages/dataBase.sql
@@ -15,3 +15,17 @@ CREATE TABLE something
 );
 
 -- create a trigger/function which will calculate the age on insert or modify given the birthday
+CREATE OR REPLACE FUNCTION update_age()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    NEW.age := DATE_PART('year', CURRENT_DATE) - DATE_PART('year', NEW.birthday);
+    RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_age_trigger
+BEFORE INSERT OR UPDATE ON something
+FOR EACH ROW
+EXECUTE FUNCTION update_age();
